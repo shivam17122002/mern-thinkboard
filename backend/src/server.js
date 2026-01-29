fffa5624be60559080a93e1d10082074807adc0a
shivam17122002 <shivmpandey02@gamil.com>
2026-01-30 01:15:53 +0530

    Resolve merge conflict

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ import rateLimiter from "./middleware/rateLimiter.js";
 import notesRoutes from "./routes/notesRoutes.js";
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+
 
 
 const __filename = fileURLToPath(import.meta.url);
