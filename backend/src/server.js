9e878681e29ea149dfe4b936a5b8ac0d005eb863
Shivam Pandey <142995456+shivam17122002@users.noreply.github.com>
2026-01-29 20:48:02 +0530

    Change server port from 3000 to 5001

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,7 @@ const app = express();
 const connectDb = require("./config/db");
 const rateLimiter = require("./middleware/rateLimiter");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
 
 connectDb();
 
