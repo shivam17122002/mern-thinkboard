7ab476fe7089ed301070e31872eda9776eabc9cf
Shivam Pandey <142995456+shivam17122002@users.noreply.github.com>
2026-04-27 21:18:32 +0530

    Change rate limiter settings in upstash.js

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -13,7 +13,7 @@ try {
 
   ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(100, "40s"),
+    limiter: Ratelimit.slidingWindow(70, "1m"),
   });
 } catch (err) {
   console.warn("[Upstash] Not configured — using no-op rate limiter for development.");
